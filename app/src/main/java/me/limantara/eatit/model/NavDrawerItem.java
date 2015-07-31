74adc645aaf697fb3c31459046966202654a8022
Limantara <edwin@limantara.me>
2015-07-31 10:42:12 -0700

    Add slide drawer

---
+++ app/src/main/java/me/limantara/eatit/model/NavDrawerItem.java
@@ -0,0 +1,53 @@
+package me.limantara.eatit.model;
+
+/**
+ * Created by edwinlimantara on 7/31/15.
+ */
+public class NavDrawerItem {
+    private boolean showNotify;
+    private String title;
+
+    /**
+     * Create a new drawer item.
+     */
+    public NavDrawerItem() {
+
+    }
+
+    /**
+     * Getter method for private showNotify instance member.
+     *
+     * @return
+     */
+    public boolean isShowNotify() {
+        return showNotify;
+    }
+
+    /**
+     * Setter method for private showNotify instance member.
+     *
+     * @param showNotify
+     */
+    public void setShowNotify(boolean showNotify) {
+        this.showNotify = showNotify;
+    }
+
+    /**
+     * Getter method for private title instance member.
+     *
+     * @return
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * Setter method for private title instance member.
+     *
+     * @param title
+     * @return
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
