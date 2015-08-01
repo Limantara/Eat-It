b73772bb74296d6efc8bf4d7cbb5e9a01e0b3ac5
Limantara <edwin@limantara.me>
2015-08-01 12:13:40 -0700

    Add menu

--- app/src/main/java/me/limantara/eatit/activity/DisplayResult.java
+++ app/src/main/java/me/limantara/eatit/activity/DisplayResult.java
@@ -2,6 +2,7 @@ package me.limantara.eatit.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -13,6 +14,10 @@ public class DisplayResult extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_result);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_reply);
+        setSupportActionBar(toolbar);
     }
 
     @Override
