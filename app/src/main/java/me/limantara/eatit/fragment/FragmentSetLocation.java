ad280c94a250b01b3e909ba5f6838e00fddbec09
Limantara <edwin@limantara.me>
2015-08-05 12:59:21 -0700

    Update set location

--- app/src/main/java/me/limantara/eatit/fragment/FragmentSetLocation.java
+++ app/src/main/java/me/limantara/eatit/fragment/FragmentSetLocation.java
@@ -1,19 +1,23 @@
 package me.limantara.eatit.fragment;
 
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import me.limantara.eatit.R;
+import me.limantara.eatit.app.AppController;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FragmentSetLocation extends Fragment {
 
+    private AppController app = AppController.getInstance();
+    private TextView cityName;
 
     public FragmentSetLocation() {
         // Required empty public constructor
@@ -23,9 +27,16 @@ public class FragmentSetLocation extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_set_location, container, false);
+
+        View root = inflater.inflate(R.layout.fragment_set_location, container, false);
+        cityName = (TextView) root.findViewById(R.id.city_name);
+
+        if(app.hasLocation())
+            cityName.setText(app.getAddress());
+
+        return root;
     }
+
 
 
 }
